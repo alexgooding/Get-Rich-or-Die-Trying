9336cc83f040b9add1642ca9c3c52557b840fdfa
Alex Gooding <ag2303@bath.ac.uk>
2017-11-16 19:40:26 +0000

    Add files via upload
    Beginning of a test class for the whole dungeon.

---
+++ DungeonTest.java
@@ -0,0 +1,6 @@
+public class DungeonTest{
+	Dungeon testDungeon = new Dungeon(100, 1);
+	for(int i=0; i < testDungeon.getNumberOfRooms(); i++){
+		testDungeon.getRooms().get(i).getRoomBoundaries()
+	}
+}
