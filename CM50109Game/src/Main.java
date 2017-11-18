20ee8320951e0e68a7791b52d53a9df658ccdc57
Kimberley Chong <klzc20@bath.ac.uk>
2017-11-18 14:12:41 +0000

    Changed Main.java
    Fixed file.

--- CM50109Game/src/Main.java
+++ CM50109Game/src/Main.java
@@ -1,67 +1,128 @@
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-
-
-import org.lwjgl.glfw.GLFWVidMode;
-
 import static org.lwjgl.opengl.GL11.*;
-import org.lwjgl.opengl.GL;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class Main {
-	private Player player;
-	
-	public static void main(String[] args) {
-		
-		if(!glfwInit()) {
-			throw new IllegalStateException("Failed to initialize GLFW!");
-		}
 
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-		
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-		long window = glfwCreateWindow(640, 480, "My CM50109 Game", 0, 0);
-		if(window ==0) {
-			throw new IllegalStateException("Faild to create window");
-		}
-		
-		glfwMakeContextCurrent(window);
-		GL.createCapabilities();	//Compatible for all versions
-		
-		GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor()) ;
-		glfwSetWindowPos(window, (videoMode.width() -640) / 2, (videoMode.height() - 480) / 2);
-		
-		
-			glfwShowWindow(window);
-			
+	// The window handle
+	private long window;
 
-		
-		while(! glfwWindowShouldClose(window)) {
-			//glfwPollEvents();
-			
-			//glClear(GL_COLOR_BUFFER_BIT);	//Need it, Otherwise rendering on top of the scene for the previous
-			
-			
-			//glfwSwapBuffers(window);	//Swaps the front and back buffers of the specified window
-			// Clear the screen and depth buffer
-			System.out.println("AAA");
-			// clear the screen and depth buffer
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-			         
-			// set the color of the quad (R,G,B,A)
-			//glColor3f(0.5f,0.5f,1.0f);
-			     
-			// draw quad
-			glBegin(GL_QUADS);
-			    glVertex2f(100,100);
-			    glVertex2f(100+200,100);
-			    glVertex2f(100+200,100+200);
-			    glVertex2f(100,100+200);
-			glEnd();
-		}
+	public void run() {
+		System.out.println("Game is running on version " + Version.getVersion() + "!");
 
+		init();
+		loop();
+
+		// Free the window callbacks and destroy the window
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+
+		// Terminate GLFW and free the error callback
 		glfwTerminate();
+		glfwSetErrorCallback(null).free();
 	}
-	
+
+	private void init() {
+		// Setup an error callback. The default implementation
+		// will print the error message in System.err.
+		GLFWErrorCallback.createPrint(System.err).set();
+
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if ( !glfwInit() )
+			throw new IllegalStateException("Unable to initialize GLFW");
+
+		// Configure GLFW
+		glfwDefaultWindowHints(); // optional, the current window hints are already the default
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+		// Create the window
+		window = glfwCreateWindow(800, 600, "Julian Dungeon", NULL, NULL);
+		if ( window == NULL )
+			throw new RuntimeException("Failed to create the GLFW window");
+
+		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
+		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+		});
+
+		// Get the thread stack and push a new frame
+		try ( MemoryStack stack = stackPush() ) {
+			IntBuffer pWidth = stack.mallocInt(1); // int*
+			IntBuffer pHeight = stack.mallocInt(1); // int*
+
+			// Get the window size passed to glfwCreateWindow
+			glfwGetWindowSize(window, pWidth, pHeight);
+
+			// Get the resolution of the primary monitor
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+			// Center the window
+			glfwSetWindowPos(
+				window,
+				(vidmode.width() - pWidth.get(0)) / 2,
+				(vidmode.height() - pHeight.get(0)) / 2
+			);
+		} // the stack frame is popped automatically
+
+		// Make the OpenGL context current
+		glfwMakeContextCurrent(window);
+		// Enable v-sync
+		glfwSwapInterval(1);
+
+		// Make the window visible
+		glfwShowWindow(window);
+	}
+
+	private void loop() {
+		// This line is critical for LWJGL's interoperation with GLFW's
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the GLCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		GL.createCapabilities();
+
+		// init OpenGL
+	    GL11.glMatrixMode(GL11.GL_PROJECTION);
+	    GL11.glLoadIdentity();
+	    GL11.glOrtho(0, 800, 0, 600, 1, -1);
+	    GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+		// Run the rendering loop until the user has attempted to close
+		// the window or has pressed the ESCAPE key.
+		while ( !glfwWindowShouldClose(window) ) {
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+			
+			// Set the clear color
+		    GL11.glColor3f(0.4f,0.4f,0.4f);
+			
+			// draw quad
+	        GL11.glBegin(GL11.GL_QUADS);
+	            GL11.glVertex2f(0,0); // top left
+	        GL11.glVertex2f(800,0); // top right
+	        GL11.glVertex2f(800,600); // bottom right
+	        GL11.glVertex2f(0,600); // bottom left
+	        GL11.glEnd();
+			glfwSwapBuffers(window); // swap the color buffers
+			
+			// Poll for window events. The key callback above will only be
+			// invoked during this call.
+			glfwPollEvents();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Main().run();
+	}
+
 }
