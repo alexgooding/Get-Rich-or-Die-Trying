f564c34d03776a3b28b1dcad3dce95b9181c7772
Alex Gooding <ag2303@bath.ac.uk>
2017-11-25 18:02:07 +0000

    Add files via upload

--- Dungeon.java
+++ Dungeon.java
@@ -1,126 +1,153 @@
+import java.util.*;
 import java.util.Random;
 import java.util.ArrayList;
 
 public class Dungeon{
 
 	/*
-	 * numberOfRooms is an integer representing how many rooms we want to generate.
-	 * dungeonDifficulty is an integer from 1-3 representing the difficulty of the dungeon.
-	 * rooms is a list of all the rooms stored in the dungeon.
-	 */
+	* numberOfRooms is an integer representing how many rooms we want to generate.
+	* dungeonDifficulty is an integer from 1-3 representing the difficulty of the dungeon.
+	* rooms is a list of all the rooms stored in the dungeon.
+	* dungeonBoundaries is a list of all the boundaries of the dungeon without doors.
+	* dungeonDoorLocations is a list of all the door locations in the dungeon.
+	*/
 	private int numberOfRooms;
 	private int dungeonDifficulty;
 	private ArrayList<Room> rooms = new ArrayList<Room>();
+	private ArrayList<Location> dungeonWalls = new ArrayList<Location>();
+	private ArrayList<Location> dungeonDoorLocations = new ArrayList<Location>();
 	private int roomCounter = 1;
 
 	// =========================
-  // Constructors
-  // =========================
+	// Constructors
+	// =========================
 
-  /**
-   * Default constructor - this initialises numberOfRooms and dugeonDifficulty to (10, 1).
-   */
+	/**
+	* Default constructor - this initialises numberOfRooms and dugeonDifficulty to (10, 1).
+	*/
 	public Dungeon(){
 		setDungeon(10, 1);
 	}
 
 	/**
-   * Four-parameter version of the constructor. Initialiases (numberOfRooms, dungeonDifficulty) to
-   * a dungeon, which is supplied to the function.
-   *
-   * @param numberOfRooms     - the number of rooms in the dungeon.
-   * @param dungeonDifficulty - the difficulty of the dungeon.
-   */
+	* Four-parameter version of the constructor. Initialiases (numberOfRooms, dungeonDifficulty) to
+	* a dungeon, which is supplied to the function.
+	*
+	* @param numberOfRooms     - the number of rooms in the dungeon.
+	* @param dungeonDifficulty - the difficulty of the dungeon.
+	*/
 	public Dungeon(int numberOfRooms, int dungeonDifficulty){
 		setDungeon(numberOfRooms, dungeonDifficulty);
 	}
 
-  // =========================
-  // Mutators and Accessors
-  // =========================
-    
-  /**
-   * Mutator for instance variables - sets the room parameters.
-   *
-   * @param numberOfRooms     - new number of rooms.
-   * @param dungeonDifficulty - new difficulty of dungeon.
-   */
-  public void setDungeon(int numberOfRooms, int dungeonDifficulty){
-  	this.numberOfRooms = numberOfRooms;
-  	this.dungeonDifficulty = dungeonDifficulty;
+	// =========================
+	// Mutators and Accessors
+	// =========================
 
-  	Room firstRoom = new Room();
-  	rooms.add(firstRoom);
-  	generateRooms(firstRoom);
-  }
+	/**
+	* Mutator for instance variables - sets the room parameters.
+	*
+	* @param numberOfRooms     - new number of rooms.
+	* @param dungeonDifficulty - new difficulty of dungeon.
+	*/
+	public void setDungeon(int numberOfRooms, int dungeonDifficulty){
+		this.numberOfRooms = numberOfRooms;
+		this.dungeonDifficulty = dungeonDifficulty;
 
-  /**
-   * Accessor for numberOfRooms. 
-   *
-   * @param  none.
-   * @return The number of rooms.
-   */   
+		Room firstRoom = new Room();
+		rooms.add(firstRoom);
+		generateRooms(firstRoom);
+		storeDungeonWalls();
+	}
+
+
+	/**
+	* Accessor for numberOfRooms. 
+	*
+	* @param  none.
+	* @return The number of rooms.
+	*/   
 	public int getNumberOfRooms(){
 		return numberOfRooms;
 	}
 
-  /**
-   * Accessor for dungeonDifficulty. 
-   *
-   * @param  none.
-   * @return The difficulty of the dungeon.
-   */   
+	/**
+	* Accessor for dungeonDifficulty. 
+	*
+	* @param  none.
+	* @return The difficulty of the dungeon.
+	*/   
 	public int getDungeonDifficulty(){
 		return dungeonDifficulty;
 	}
 
 	/**
-   * Accessor for rooms. 
-   *
-   * @param  none.
-   * @return The list of rooms.
-   */   
+	* Accessor for rooms. 
+	*
+	* @param  none.
+	* @return The list of rooms.
+	*/   
 	public ArrayList<Room> getRooms(){
 		return rooms;
 	}
 
-	// =========================
-  // Additional Methods
-  // =========================
+	/**
+	* Accessor for dungeonWalls. 
+	*
+	* @param  none.
+	* @return The list of dungeon boundaries.
+	*/  
+	public ArrayList<Location> getDungeonWalls(){
+		return dungeonWalls;
+	}
 
 	/**
-   * Generates rooms of the same size with a random amount of doors and in a random pattern. 
-   * The rooms are stored in the rooms list. 
-   *
-   * @param  initialRoom - the previous room in the dungeon complex.
-   * @return void.
-   */  
-  public void generateRooms(Room initialRoom){
-  	for(int i=0; i<initialRoom.getNumberOfDoors(); i++){
-  		Location newLocation = new Location();
-  		switch(initialRoom.getDoorLocations().get(i).getDirection()){
-  			case 's':
-  				newLocation = new Location(initialRoom.getRoomLocation().getX(), initialRoom.getRoomLocation().getY()-initialRoom.getRoomSize(), 1);
-  				break;
-  			case 'e':
-  				newLocation = new Location(initialRoom.getRoomLocation().getX()+initialRoom.getRoomSize(), initialRoom.getRoomLocation().getY(), 1);
-  				break;
-  			case 'n':
-  				newLocation = new Location(initialRoom.getRoomLocation().getX(), initialRoom.getRoomLocation().getY()+initialRoom.getRoomSize(), 1);
-  				break;	
-  			case 'w':
-  				newLocation = new Location(initialRoom.getRoomLocation().getX()-initialRoom.getRoomSize(), initialRoom.getRoomLocation().getY(), 1);
-  				break;
-  		}
-  		boolean repeatFlag = false; //Flags whether the room already exists in the rooms list.
-  		for(int j=0; j<rooms.size(); j++){
-  			repeatFlag = newLocation.equals(rooms.get(j).getRoomLocation());
-  			if(repeatFlag == true){
-  				j = rooms.size();
-  			}
-  		}
-  		if(repeatFlag==false){
-  			Random rand = new Random();
+	* Accessor for dungeonDoorLocations. 
+	*
+	* @param  none.
+	* @return The list of the locations of all the doors in the dungeon.
+	*/  
+	public ArrayList<Location> getDungeonDoorLocations(){
+		return dungeonDoorLocations;
+	}	
+
+	// =========================
+	// Additional Methods
+	// =========================
+
+	/**
+	* Generates rooms of the same size with a random amount of doors and in a random pattern. 
+	* The rooms are stored in the rooms list. 
+	*
+	* @param  initialRoom - the previous room in the dungeon complex.
+	* @return void.
+	*/  
+	public void generateRooms(Room initialRoom){
+		for(int i=0; i<initialRoom.getNumberOfDoors(); i++){
+			Location newLocation = new Location();
+			switch(initialRoom.getDoorLocations().get(i).getDirection()){
+				case 's':
+					newLocation = new Location(initialRoom.getRoomLocation().getX(), initialRoom.getRoomLocation().getY()-initialRoom.getRoomSize(), 1);
+					break;
+				case 'e':
+					newLocation = new Location(initialRoom.getRoomLocation().getX()+initialRoom.getRoomSize(), initialRoom.getRoomLocation().getY(), 1);
+					break;
+				case 'n':
+					newLocation = new Location(initialRoom.getRoomLocation().getX(), initialRoom.getRoomLocation().getY()+initialRoom.getRoomSize(), 1);
+					break;	
+				case 'w':
+					newLocation = new Location(initialRoom.getRoomLocation().getX()-initialRoom.getRoomSize(), initialRoom.getRoomLocation().getY(), 1);
+					break;
+			}
+			boolean repeatFlag = false; //Flags whether the room already exists in the rooms list.
+			for(int j=0; j<rooms.size(); j++){
+				repeatFlag = newLocation.equals(rooms.get(j).getRoomLocation());
+				if(repeatFlag == true){
+					j = rooms.size();
+				}
+			}
+			if(repeatFlag==false){
+				Random rand = new Random();
 				//int randomDoorNumber = rand.nextInt(4);
 				double nextDoorNumber = rand.nextGaussian()*1+1.5;
 				int randomDoorNumber; 
@@ -131,15 +158,43 @@ public class Dungeon{
 				if(randomDoorNumber<0){
 					randomDoorNumber = 0;
 				}
-  			Room newRoom = new Room(initialRoom.getRoomSize(), newLocation, randomDoorNumber, 1);
-  			if(roomCounter>=numberOfRooms){ //Limits the number of rooms to the correct amount.
-  				return;
-  			}
-  			rooms.add(newRoom);
-  			roomCounter++;
-  			generateRooms(newRoom);
-  		}
-  	}
+				Room newRoom = new Room(initialRoom.getRoomSize(), newLocation, randomDoorNumber, 1);
+				if(roomCounter>=numberOfRooms){ //Limits the number of rooms to the correct amount.
+					return;
+				}
+				rooms.add(newRoom);
+				roomCounter++;
+				generateRooms(newRoom);
+			}
+		}
+	}
+
+	/**
+	* Stores the boundaries of the dungeon without doors.
+	* 
+	*
+	* @return void.
+	*/  
+	public void storeDungeonWalls(){
+		for(int i=0; i<getRooms().size(); i++){
+		  	for(int j=0; j<getRooms().get(i).getRoomBoundaries().length; j++){
+		    	dungeonWalls.add(getRooms().get(i).getRoomBoundaries()[j]);
+		    }
+		} 
+
+		for(int i=0; i<getRooms().size(); i++){
+		  	for(int j=0; j<getRooms().get(i).getDoorLocations().size(); j++){
+		    	dungeonDoorLocations.add(getRooms().get(i).getDoorLocations().get(j).getDoorLocation());
+		  	}
+		}
+
+		for(int i=0; i<dungeonWalls.size(); i++){
+		  	for(int j=0; j<dungeonDoorLocations.size(); j++){
+		    	if(dungeonWalls.get(i).equals(dungeonDoorLocations.get(j)) == true){
+		      		dungeonWalls.remove(i);
+		    	}
+		  	}
+		}
 	}
 }
 
--- LocationTest.java
+++ LocationTest.java
@@ -1,33 +1,33 @@
 public class LocationTest{ 
     
-  //Tester for the location class 
-  public static void main(String[] args){
-    //Can we define a location?
-    Location testLocation1 = new Location(1,3,1);
-    
-    //Can the location be printed?
-    System.out.println("The testLocation1 is: ");
-    testLocation1.printLocation();
-    
-    //Can we return the co-ordinates of a room?
-    System.out.println("The x co-ordinate of testLocation1 is: " + testLocation1.getX());
-    System.out.println("The y co-ordinate of testLocation1 is: " + testLocation1.getY());
-    
-    //Can the room number be returned?
-    System.out.println("The room number testLocation1 is: " + testLocation1.getRoomNumber());
-    
-    //Can the location be updated - i.e. step to the right?
-    testLocation1 = new Location((testLocation1.getX() + 1), testLocation1.getY(), testLocation1.getRoomNumber());
-    System.out.println("We move one step to the right along the x axis, the new location is: "); 
-    testLocation1.printLocation();
-    
-    //Can negative numbers be stored as a room number?
-    Location testLocation2 = new Location(1,6,-1);
-    System.out.println("The testLocation2 is: "); 
-    testLocation2.printLocation();
-    
-    //Can we return the distance between two locations?
-    System.out.println("The distance between testLocation1 and testLocation2 is : " + testLocation1.distanceFrom(testLocation2));
-  }
+  	//Tester for the location class 
+  	public static void main(String[] args){
+		//Can we define a location?
+		Location testLocation1 = new Location(1,3,1);
+
+		//Can the location be printed?
+		System.out.println("The testLocation1 is: ");
+		testLocation1.printLocation();
+
+		//Can we return the co-ordinates of a room?
+		System.out.println("The x co-ordinate of testLocation1 is: " + testLocation1.getX());
+		System.out.println("The y co-ordinate of testLocation1 is: " + testLocation1.getY());
+
+		//Can the room number be returned?
+		System.out.println("The room number testLocation1 is: " + testLocation1.getRoomNumber());
+
+		//Can the location be updated - i.e. step to the right?
+		testLocation1 = new Location((testLocation1.getX() + 1), testLocation1.getY(), testLocation1.getRoomNumber());
+		System.out.println("We move one step to the right along the x axis, the new location is: "); 
+		testLocation1.printLocation();
+
+		//Can negative numbers be stored as a room number?
+		Location testLocation2 = new Location(1,6,-1);
+		System.out.println("The testLocation2 is: "); 
+		testLocation2.printLocation();
+
+		//Can we return the distance between two locations?
+		System.out.println("The distance between testLocation1 and testLocation2 is : " + testLocation1.distanceFrom(testLocation2));
+	}
 
 } 
