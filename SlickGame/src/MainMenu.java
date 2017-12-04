16dd34adaae4a94977a599f3f30110ea8a5d0a02
Kimberley <klzc20@bath.ac.uk>
2017-12-04 00:27:37 +0000

    Changed input methods to Slick methods instead of using lwjgl methods

--- SlickGame/src/MainMenu.java
+++ SlickGame/src/MainMenu.java
@@ -1,13 +1,8 @@
 import java.awt.Font;
 import java.io.InputStream;
 
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Color;
-import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.util.ResourceLoader;
 
 //version v1.0: Ray: Initial Create
@@ -34,6 +29,7 @@ public class MainMenu extends BasicGameState {
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		g.translate(-520,-320);
 		// Set title on the screen
 		float titleWidth = titleFont.getWidth(Main.gameName);
 		titleFont.drawString((Main.winWidth - titleWidth) / 2, 200, Main.gameName, Color.white);
@@ -44,29 +40,31 @@ public class MainMenu extends BasicGameState {
 		float buttonCenter = (Main.winWidth - 300) / 2;
 		startGameButton.draw(buttonCenter, Main.halfHeight + 20);
 		quitButton.draw(buttonCenter, Main.halfHeight + 170);
+		g.resetTransform();
 	}
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-		int mousePosX = Mouse.getX();
+		Input input = gc.getInput();
+		int mousePosX = input.getMouseX();
 		// Y position usually starts from bottom but this starts from the top
-		int mousePosY = (Main.winHeight - Mouse.getY());
+		int mousePosY = input.getMouseY();
 		// Get the coordinates for the centering of buttons for accuracy of mouse clicks
 		float buttonCenter = (Main.winWidth - 300) / 2;
 		System.out.println("X: " + mousePosX + " Y: " + mousePosY);
 		// If Start Game button is pressed, start the game.
 		if((mousePosX > buttonCenter) && (mousePosX < buttonCenter +  300) && (mousePosY >  Main.halfHeight + 20 && mousePosY < Main.halfHeight + 500)) {
-			if(Mouse.isButtonDown(0)) {
+			if(input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
 				sbg.enterState(1);
 			}
 		}
 		// If Quit button is pressed, the window is closed
 		if((mousePosX > buttonCenter && mousePosX < buttonCenter + 300) && (mousePosY > Main.halfHeight + 170 && mousePosY < 650)) {
-			if(Mouse.isButtonDown(0)) {
+			if(input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
 				System.exit(0);
 			}
 		}
 		// Enters the game if Enter key is pressed
-		if (Keyboard.isKeyDown(Input.KEY_ENTER)) {
+		if (input.isKeyDown(Input.KEY_ENTER)) {
 			sbg.enterState(1);
 		}
 	}
